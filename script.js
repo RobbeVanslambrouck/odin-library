549b287d58a5e86c78ea9774e5af694d0c4bc678
Robbe Vanslambrouck <robbevsb.dev@gmail.com>
2022-06-07 21:53:52 +0200

    basic design done

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hello");
+let myLibrary = [];
+
+function Book(title, author, NumOfPages, haveRead) {
+    this.title = title;
+    this.author = author
+    this.pages = NumOfPages;
+    this.read = haveRead;
+    this.info = function() {
+        return this.title + " by " + this.author + ", " + this.pages + " pages, " + (this.read ? "already read" : "not read yet");
+    }
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
