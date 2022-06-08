4d1bb8e901f017a36d8b34fe759aa6e57707c24b
Robbe Vanslambrouck <robbevsb.dev@gmail.com>
2022-06-08 21:47:03 +0200

    small code cleaning

--- script.js
+++ script.js
@@ -1,46 +1,51 @@
+const sectionAddBook = document.querySelector("#add-book")
+const sectionAddBookBtnCancel = document.querySelector("#cancel")
+const formAddBook = document.querySelector("form");
+
 let myLibrary = [];
 
 function Book(title, author, NumOfPages, haveRead) {
     this.title = title;
     this.author = author
     this.pages = NumOfPages;
-    this.read = haveRead;
+    this.isRead = haveRead;
     this.info = function() {
-        return this.title + " by " + this.author + ", " + this.pages + " pages, " + (this.read ? "already read" : "not read yet");
+        return this.title + " by " + this.author + ", " + this.pages + " pages, " + (this.isRead ? "already read" : "not read yet");
     }
 }
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
+    saveLibraryLocally();
 }
 
-function BookCard(id, title, author, pages, read) {
+function BookCard(bookId, title, author, pages, isRead) {
     const divBook = document.createElement("article");
     const h3Title = document.createElement("h3");
     const pAuthor = document.createElement("p");
     const btnRemove = document.createElement("button");
     const iTrash = document.createElement("i");
     const pPages = document.createElement("p");
-    const btnRead = document.createElement("button");
+    const btnIsRead = document.createElement("button");
     divBook.className = "book";
-    divBook.id = id;
+    divBook.id = bookId;
     h3Title.className = "title";
     pAuthor.className = "author";
     btnRemove.className = "remove";
     iTrash.className = "fa-solid fa-trash";
     pPages.className = "pages";
-    btnRead.className = "status";
+    btnIsRead.className = "status";
     btnRemove.type = "button";
-    btnRead.type = "button";
+    btnIsRead.type = "button";
     btnRemove.append(iTrash);
-    divBook.append(h3Title, pAuthor, btnRemove, pPages, btnRead)
+    divBook.append(h3Title, pAuthor, btnRemove, pPages, btnIsRead)
     btnRemove.onclick = clickBookRemove;
-    btnRead.onclick = clickReadStatus;
+    btnIsRead.onclick = clickReadStatus;
 
     h3Title.textContent = title;
     pAuthor.textContent = author;
     pPages.textContent = pages;
-    btnRead.textContent = read ? "read" : "not read";
+    btnIsRead.textContent = isRead ? "read" : "not read";
 
     return divBook;
 }
@@ -56,45 +61,32 @@ function displayLibrary(library) {
     });
 }
 
-const formAddBook = document.querySelector("form");
-const sectionAddBook = document.querySelector(".add-book-hidden")
-const sectionAddBookBtnCancel = document.querySelector("#cancel")
-
-function clickAddBookBtn(e) {
+function clickShowAddBookBtn(e) {
     e.stopPropagation();
-    sectionAddBook.removeEventListener("click", clickAddBookBtn);
+    sectionAddBook.removeEventListener("click", clickShowAddBookBtn);
     sectionAddBook.className = "add-book";
 }
 
 function clickAddBookCancel(e) {
     e.stopPropagation();
-    sectionAddBook.addEventListener("click", clickAddBookBtn);
+    sectionAddBook.addEventListener("click", clickShowAddBookBtn);
     sectionAddBook.className = "add-book-hidden";
 }
 
-function handleSubmitAddBook(e) {
+function submitAddBook(e) {
     e.preventDefault();
-    e.stopPropagation();
-    sectionAddBook.addEventListener("click", clickAddBookBtn);
+    sectionAddBook.addEventListener("click", clickShowAddBookBtn);
     sectionAddBook.className = "add-book-hidden";
-
+    
     const inputTitle = document.querySelector("input#title");
     const inputAuthor = document.querySelector("input#author");
     const inputPages = document.querySelector("input#pages");
     const inputRead = document.querySelector("input#read");
-
-    console.log(inputRead);
+    
     let newBook = new Book(inputTitle.value, inputAuthor.value, parseInt(inputPages.value), inputRead.checked)
     addBookToLibrary(newBook);
     displayLibrary(myLibrary);
-    saveLibraryLocally();
 }
-
-sectionAddBook.addEventListener("click", clickAddBookBtn);
-sectionAddBookBtnCancel.addEventListener("click", clickAddBookCancel);
-formAddBook.onsubmit = handleSubmitAddBook;
-
-const btnBookRemove = document.querySelectorAll("article.book .remove");
 
 function clickBookRemove(e) {
     e.stopPropagation;
@@ -127,5 +119,13 @@ function getLibraryFormLocalStorage() {
     return JSON.parse(j);
 }
 
-myLibrary = getLibraryFormLocalStorage();
-displayLibrary(myLibrary);
+function app() {
+    sectionAddBook.addEventListener("click", clickShowAddBookBtn);
+    sectionAddBookBtnCancel.addEventListener("click", clickAddBookCancel);
+    formAddBook.onsubmit = submitAddBook;
+    
+    myLibrary = getLibraryFormLocalStorage();
+    displayLibrary(myLibrary);
+}
+
+app();
